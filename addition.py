e23556a1176a0a040081b729147f128d392b8359
Harish-27-R <77011144+Harish-27-R@users.noreply.github.com>
2021-01-12 11:54:43 +0530

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,8 @@
+print("\tThe program for addition operation" )
+a=int(input("enter first number : \t"))
+b=int(input("enter second number : \t"))
+c=a+b
+print("\tThe program for addition operation" )
+print ("\tThe a value is ",a)
+print("\tThe value is ",b)
+print ("\tThe addition of a & b is ",c)
