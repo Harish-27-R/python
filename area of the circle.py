4b58008f08c012e303544c36644cf5ad629ff3b2
Harish-27-R <77011144+Harish-27-R@users.noreply.github.com>
2021-01-12 12:40:21 +0530

    Add files via upload

---
+++ area of the circle.py
@@ -0,0 +1,5 @@
+print("The Python Program to Find the Area of the Circle")
+radius=int(input("Enter the radius of the circle :\t")) #Receives the radius  value from the user
+area=3.14*radius*radius #performs the Area of the circle calculation
+print("The given radius of the circle is :\t",radius) #Displays the Radius value given by the user
+print("The area of the circle for the given radius is :",area)#Displays the Area of circle 
