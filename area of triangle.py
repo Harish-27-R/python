9248d37a386e340f7521847ff0c913dceb0c08a7
Harish-27-R <77011144+Harish-27-R@users.noreply.github.com>
2021-01-12 12:47:28 +0530

    Add files via upload

---
+++ area of triangle.py
@@ -0,0 +1,7 @@
+print("The Python Program to find Area of the triangle ")
+base=int(input("Enter the base value of the triangle :\t")) #Receives the base value of the triangle from the user
+height=int(input("Enter the height of the triangle :\t"))#Receives the heigth of the triangle from the user
+area=(height*base)/2#Performs the area of the triangle calculation
+print("The height of the triangle is :",heigth)#Displays the height of the  triangle given by the user
+print("The base  of the triangle is :",base)#Displays the base of the triangle  given by the user
+print("The Area of the triangle for the given base & height value is :\t",area)#Displays the area of the Triangle For the user defined input
