28449dc82f5fdf7105e7ecc1e2bf4bf24dbe83fe
Harish-27-R <77011144+Harish-27-R@users.noreply.github.com>
2021-01-12 12:34:23 +0530

    Add files via upload

---
+++ area of rectangle.py
@@ -0,0 +1,7 @@
+print("Program to find the area of rectangle")
+length=int(input("Enter the length of the rectangle :\t")) #Receives the length of the rectaangle from the user
+breath=int(input("Enter the width of the rectangle :\t")) # Receives the breath of the rectangle from the user
+area=length *breath #performs the calculation
+print("The width of the rectangle is :",breath)#displays the breath of the rectangle given by the user
+print("The length of the rectangle is :",length)#displays the length of the  rectangle given by the  user
+print("The area of the rectangle for the given length & width is : ",area))#displays the area of the rectangle for the user defined input
