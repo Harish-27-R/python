f1cb22634a4887fc35c1337ea84a00caf594767b
Harish-27-R <77011144+Harish-27-R@users.noreply.github.com>
2021-01-12 12:11:59 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -1,8 +1,7 @@
 print("\tThe program for addition operation" )
-a=int(input("enter first number : \t"))
-b=int(input("enter second number : \t"))
-c=a+b
-print("\tThe program for addition operation" )
-print ("\tThe a value is ",a)
-print("\tThe value is ",b)
-print ("\tThe addition of a & b is ",c)
+operand1=int(input("Enter first number : \t")) #Receives The Input from the user(i.e.First operand)
+operand2=int(input("Enter second number : \t")) # Receives  The Input from the user(i.e.Second  operand)
+result=operand1+operand2 #performs the addition operation
+print ("\tThe First Operand  is ",operand1) # Displays the First operand
+print("\tThe Second Operand  is ",operand2) # Displays the Second operand
+print ("\tThe addition of a & b is ",result) # Displays the result (i.e. The addition of operand 1 & operand 2)
