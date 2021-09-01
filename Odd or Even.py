66e4057032e063b9d3cc343e4e3159ba17887f8d
Harish-27-R <77011144+Harish-27-R@users.noreply.github.com>
2021-09-01 10:05:16 +0530

    Add files via upload

---
+++ Odd or Even.py
@@ -0,0 +1,6 @@
+num=int(input("Enter the number to check whether it is odd or even :"))
+a=num%2
+if(a==0):
+    print("It is a Even Number")
+else:
+    print("It is an odd number")
---
+++ comparison operator.py
@@ -0,0 +1,6 @@
+valye1=input("Enter a number : ")
+value2=input("Enter another number : ")
+if(a>b):
+    print("The value " " is greater",a)
+    else:
+    print("The value " " is greater",b)
